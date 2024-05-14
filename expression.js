54837b570d83a4c4b2110ad7dedf330a5846a8ba
oilly18 <oilly18@hotmail.com>
2024-05-14 15:44:03 +0700

    firstcommit

---
+++ expression.js
@@ -0,0 +1,16 @@
+//2.1
+// //Expression
+function evenNumber(x) {
+    x % 2 === 0 ? console.log('even') : console.log('odd')
+};
+
+evenNumber(5);
+
+
+
+
+//2.2
+
+const checkEven = x =>  x % 2 === 0 ? console.log('even') : console.log('odd');
+
+checkEven(6);
